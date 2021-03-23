b65b4654d328f25ceebb09ddcc8146f419c3c94f
christinadae <christina.daeschner@hs-furtwangen.de>
2021-03-23 15:05:02 +0100

    edit

--- L01_Poem/POEM.js
+++ L01_Poem/POEM.js
@@ -4,8 +4,21 @@ var randomPoem;
     let subjects = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
     let predicates = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
     let objects = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
-    for (let index = 0; index <= subjects.length; index--) {
+    console.log(subjects);
+    console.log(predicates);
+    console.log(objects);
+    for (let index = 6; subjects.length <= index; index--) {
         console.log(subjects[index]);
     }
+    /*for (let index: number = 0; index  <=  subjects.length; index++) {
+        console.log(subjects[index]);
+        }
+    for (let index: number = 0; index  <=  predicates.length; index++) {
+            console.log(predicates[index]);
+        }
+    for (let index: number = 0; index  <=  objects.length; index++) {
+            console.log(objects[index]);
+        }
+    */
 })(randomPoem || (randomPoem = {}));
 //# sourceMappingURL=POEM.js.map
--- L01_Poem/POEM.ts
+++ L01_Poem/POEM.ts
@@ -6,9 +6,22 @@ let predicates: string[] = ["braut" , "liebt", "studiert" , "hasst", "zaubert", 
 
 let objects: string[] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
 
+console.log(subjects);
+console.log(predicates);
+console.log(objects);
 
-
-for (let index: number = 0; index  <=  subjects.length; index--) {
+for (let index: number = 6; subjects.length  <= index; index --) {
     console.log(subjects[index]);
-}
+    }
+
+/*for (let index: number = 0; index  <=  subjects.length; index++) {
+    console.log(subjects[index]);
+    }
+for (let index: number = 0; index  <=  predicates.length; index++) {
+        console.log(predicates[index]);
+    }
+for (let index: number = 0; index  <=  objects.length; index++) {
+        console.log(objects[index]);
+    }
+*/
 }
