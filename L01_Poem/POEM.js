58482517956413d1f20275de43b386994b58cfba
christinadae <christina.daeschner@hs-furtwangen.de>
2021-03-23 12:33:41 +0100

    arrays

--- L01_Poem/POEM.js
+++ L01_Poem/POEM.js
@@ -1,3 +1,8 @@
 "use strict";
-console.log("Expecto Patronum");
+var randomPoem;
+(function (randomPoem) {
+    let subjects = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+    let predicates = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+    let objects = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+})(randomPoem || (randomPoem = {}));
 //# sourceMappingURL=POEM.js.map
--- L01_Poem/POEM.ts
+++ L01_Poem/POEM.ts
@@ -1,1 +1,11 @@
-console.log("Expecto Patronum");
+namespace randomPoem {
+
+let subjects: string[] =  ["Harry" , "Hermine" , "Ron" , "Hagrid" , "Snape" , "Dumbledore"];
+
+let predicates: string[] = ["braut" , "liebt", "studiert" , "hasst", "zaubert", "zerstört"];
+
+let objects: string[] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+
+
+
+}
