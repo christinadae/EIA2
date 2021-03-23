d7acad80dc3230478e577b64d2817192c70aa163
christinadae <christina.daeschner@hs-furtwangen.de>
2021-03-23 21:06:33 +0100

    edit

--- L01_Poem/POEM.js
+++ L01_Poem/POEM.js
@@ -1,24 +1,24 @@
 "use strict";
 var randomPoem;
 (function (randomPoem) {
-    let subjects = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
-    let predicates = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+    let subjects = ["Harry ", "Hermine ", "Ron ", "Hagrid ", "Snape ", "Dumbledore "];
+    let predicates = ["braut ", "liebt ", "studiert ", "hasst ", "zaubert ", "zerstört "];
     let objects = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
-    console.log(subjects);
-    console.log(predicates);
-    console.log(objects);
-    for (let index = 6; subjects.length <= index; index--) {
-        console.log(subjects[index]);
+    //console.log(subjects);
+    //console.log(predicates);
+    //console.log(objects);
+    for (let index = 6; index > 0; index--) {
+        //console.log(index);
+        let a = getVerse(subjects, predicates, objects);
+        console.log(a);
     }
-    /*for (let index: number = 0; index  <=  subjects.length; index++) {
-        console.log(subjects[index]);
-        }
-    for (let index: number = 0; index  <=  predicates.length; index++) {
-            console.log(predicates[index]);
-        }
-    for (let index: number = 0; index  <=  objects.length; index++) {
-            console.log(objects[index]);
-        }
-    */
+    function getVerse(_subjects, _predicates, _objects) {
+        let vers = "";
+        let randomsubject = Math.floor(Math.random() * Math.floor(_subjects.length));
+        let randompredicates = Math.floor(Math.random() * Math.floor(_predicates.length));
+        let randomobject = Math.floor(Math.random() * Math.floor(_objects.length));
+        vers = _subjects[randomsubject] + _predicates[randompredicates] + _objects[randomobject];
+        return vers;
+    }
 })(randomPoem || (randomPoem = {}));
 //# sourceMappingURL=POEM.js.map
--- L01_Poem/POEM.ts
+++ L01_Poem/POEM.ts
@@ -1,27 +1,37 @@
 namespace randomPoem {
 
-let subjects: string[] =  ["Harry" , "Hermine" , "Ron" , "Hagrid" , "Snape" , "Dumbledore"];
+let subjects: string[] =  ["Harry " , "Hermine " , "Ron " , "Hagrid " , "Snape " , "Dumbledore "];
 
-let predicates: string[] = ["braut" , "liebt", "studiert" , "hasst", "zaubert", "zerstört"];
+let predicates: string[] = ["braut " , "liebt ", "studiert " , "hasst ", "zaubert ", "zerstört "];
 
 let objects: string[] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
 
-console.log(subjects);
-console.log(predicates);
-console.log(objects);
+//console.log(subjects);
+//console.log(predicates);
+//console.log(objects);
 
-for (let index: number = 6; subjects.length  <= index; index --) {
-    console.log(subjects[index]);
+
+
+for (let index: number = 6; index > 0; index --) {
+    //console.log(index);
+    
+    let a: string = getVerse(subjects, predicates, objects);
+    console.log(a);
+    
     }
 
-/*for (let index: number = 0; index  <=  subjects.length; index++) {
-    console.log(subjects[index]);
-    }
-for (let index: number = 0; index  <=  predicates.length; index++) {
-        console.log(predicates[index]);
-    }
-for (let index: number = 0; index  <=  objects.length; index++) {
-        console.log(objects[index]);
-    }
-*/
+function getVerse (_subjects: string[], _predicates: string[], _objects: string[]) {
+
+let vers: string = "";
+let randomsubject: number = Math.floor(Math.random() * Math.floor(_subjects.length));
+let randompredicates: number = Math.floor(Math.random() * Math.floor(_predicates.length));
+let randomobject: number = Math.floor(Math.random() * Math.floor(_objects.length));
+
+vers = _subjects[randomsubject] + _predicates[randompredicates] + _objects[randomobject];
+
+return vers;
+
+}
+
+
 }
