f6f2a0339787fad1dfc45de08899d22c1ddd81d1
christinadae <christina.daeschner@hs-furtwangen.de>
2021-03-23 21:29:03 +0100

    :)

--- L01_Poem/POEM.ts
+++ L01_Poem/POEM.ts
@@ -1,23 +1,16 @@
 namespace randomPoem {
 
 let subjects: string[] =  ["Harry " , "Hermine " , "Ron " , "Hagrid " , "Snape " , "Dumbledore "];
-
 let predicates: string[] = ["braut " , "liebt ", "studiert " , "hasst ", "zaubert ", "zerstört "];
-
 let objects: string[] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
 
 //console.log(subjects);
 //console.log(predicates);
 //console.log(objects);
-
-
-
 for (let index: number = 6; index > 0; index --) {
     //console.log(index);
-    
     let a: string = getVerse(subjects, predicates, objects);
     console.log(a);
-    
     }
 
 function getVerse (_subjects: string[], _predicates: string[], _objects: string[]) {
@@ -34,8 +27,5 @@ _predicates.splice(randompredicates, 1);
 _objects.splice(randomobject, 1);
 
 return vers;
-
 }
-
-
 }
