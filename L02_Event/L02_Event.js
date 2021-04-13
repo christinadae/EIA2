7fee1531f3fb472f845b0436da15fddeb5a47387
christinadae <christina.daeschner@hs-furtwangen.de>
2021-04-13 17:12:16 +0200

    l02 beginning

---
+++ L02_Event/L02_Event.js
@@ -0,0 +1,31 @@
+"use strict";
+var L02_Event;
+(function (L02_Event) {
+    window.addEventListener("load", handleLoad);
+    function handleLoad() {
+        document.addEventListener("mousemove", setinfoBox);
+        document.addEventListener("click", logInfo);
+        document.addEventListener("keyup", logInfo);
+        document.body.addEventListener("click", logInfo);
+        document.body.addEventListener("keyup", logInfo);
+        document.querySelector("#div0").addEventListener("click", logInfo);
+        document.querySelector("#div0").addEventListener("keyup", logInfo);
+        document.querySelector("#div1").addEventListener("click", logInfo);
+        document.querySelector("#div1").addEventListener("keyup", logInfo);
+    }
+    function setinfoBox(_event) {
+        let span = document.querySelector("span");
+        let x = _event.clientX;
+        let y = _event.clientY;
+        let cursorCoordinates = "X-Coordinates: " + x + ", Y-Coordinates: " + y;
+        let showTarget = _event.target;
+        span.style.left = x + 40 + "px";
+        span.style.top = y + 40 + "px";
+        span.innerHTML = cursorCoordinates + showTarget;
+    }
+    ;
+    function logInfo(_event) {
+    }
+    ;
+})(L02_Event || (L02_Event = {}));
+//# sourceMappingURL=L02_Event.js.map
---
+++ L02_Event/L02_Event.ts
@@ -0,0 +1,42 @@
+namespace L02_Event {
+
+    window.addEventListener("load", handleLoad);
+
+    function handleLoad(): void {
+
+        document.addEventListener("mousemove", setinfoBox);
+
+        document.addEventListener("click", logInfo);
+        document.addEventListener("keyup", logInfo);
+        document.body.addEventListener("click", logInfo);
+        document.body.addEventListener("keyup", logInfo);
+        document.querySelector("#div0").addEventListener("click", logInfo);
+        document.querySelector("#div0").addEventListener("keyup", logInfo);
+        document.querySelector("#div1").addEventListener("click", logInfo);
+        document.querySelector("#div1").addEventListener("keyup", logInfo);
+
+    }
+
+    function setinfoBox(_event: MouseEvent): void {
+
+        let span: HTMLSpanElement =  document.querySelector("span");
+
+        let x: number = _event.clientX;
+        let y: number = _event.clientY;
+        
+        let cursorCoordinates: string = "X-Coordinates: " + x + ", Y-Coordinates: " + y;
+
+        let showTarget = _event.target;
+
+        span.style.left = x + 40 +  "px";
+        span.style.top = y + 40 + "px";
+
+        span.innerHTML = cursorCoordinates + showTarget;
+        
+    };
+
+    function logInfo(_event: Event): void {
+
+    };
+
+}
