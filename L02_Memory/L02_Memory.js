4d9fa3d759b971d8f5a2210f26835e1695dd829d
christinadae <christina.daeschner@hs-furtwangen.de>
2021-04-21 21:26:07 +0200

    anfang

---
+++ L02_Memory/L02_Memory.js
@@ -0,0 +1,11 @@
+"use strict";
+var L02_Memory;
+(function (L02_Memory) {
+    let cards = [];
+    cards[0] = "A",
+        cards[1] = "A",
+        cards[2] = "B",
+        cards[3] = "B",
+    ;
+})(L02_Memory || (L02_Memory = {}));
+//# sourceMappingURL=L02_Memory.js.map
---
+++ L02_Memory/L02_Memory.ts
@@ -0,0 +1,25 @@
+namespace L02_Memory {
+
+let cards: string[] = [];
+
+cards[0] = "A",
+cards[1] = "A",
+cards[2] = "B",
+cards[3] = "B",
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+}
