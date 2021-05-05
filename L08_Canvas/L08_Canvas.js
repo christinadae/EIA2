cc385de1a82cfc212c2b7503f562a98bf659d3d9
christinadae <christina.daeschner@hs-furtwangen.de>
2021-05-05 18:20:21 +0200

    generative

--- L08_Canvas/L08_Canvas.js
+++ L08_Canvas/L08_Canvas.js
@@ -1,17 +1,33 @@
 "use strict";
-let canvas = document.querySelector("canvas");
-let crc2 = canvas.getContext("2d");
-/*
-crc2.fillStyle = "burlywood";
-crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
-crc2.strokeStyle = "grey";
-crc2.lineWidth = 10;
-crc2.strokeRect(0, 0, crc2.canvas.width, crc2.canvas.height);
-*/
-crc2.beginPath();
-crc2.moveTo(100, 100);
-crc2.lineTo(300, 100);
-crc2.lineTo(200, 150);
-crc2.closePath();
-crc2.stroke();
+var L08_Canvas;
+(function (L08_Canvas) {
+    window.addEventListener("load", handleLoad);
+    function handleLoad() {
+        let canvas = document.querySelector("canvas");
+        let crc2 = canvas.getContext("2d");
+        canvas.height = window.innerHeight;
+        canvas.width = window.innerWidth;
+        let colors = ["thistle", "sandybrown", "lightsalmon", "teal", "indianred"];
+        for (let index = 0; index < 50; index++) {
+            let x = Math.floor(Math.random() * Math.floor(canvas.width));
+            let y = Math.floor(Math.random() * Math.floor(canvas.height));
+            let x2 = Math.floor(Math.random() * Math.floor(canvas.width));
+            let y2 = Math.floor(Math.random() * Math.floor(canvas.height));
+            let x3 = Math.floor(Math.random() * Math.floor(canvas.width));
+            let y3 = Math.floor(Math.random() * Math.floor(canvas.height));
+            let pickColor = Math.floor(Math.random() * Math.floor(5));
+            crc2.beginPath();
+            crc2.strokeStyle = colors[pickColor];
+            crc2.moveTo(x, y);
+            crc2.lineTo(x2, y2);
+            crc2.lineTo(x3, y3);
+            crc2.closePath();
+            crc2.stroke();
+            crc2.beginPath();
+            crc2.fillStyle = colors[pickColor];
+            crc2.arc(x, y, x2, y2, 2 * Math.PI, true);
+            crc2.stroke();
+        }
+    }
+})(L08_Canvas || (L08_Canvas = {}));
 //# sourceMappingURL=L08_Canvas.js.map
--- L08_Canvas/L08_Canvas.ts
+++ L08_Canvas/L08_Canvas.ts
@@ -1,16 +1,48 @@
-let canvas: HTMLCanvasElement = <HTMLCanvasElement> document.querySelector("canvas");
-let crc2: CanvasRenderingContext2D = <CanvasRenderingContext2D> canvas.getContext("2d");
-/*
-crc2.fillStyle = "burlywood";
-crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
-crc2.strokeStyle = "grey";
-crc2.lineWidth = 10;
-crc2.strokeRect(0, 0, crc2.canvas.width, crc2.canvas.height);
-*/
-crc2.beginPath();
-crc2.moveTo(100, 100);
-crc2.lineTo(300, 100);
-crc2.lineTo(200, 150);
-crc2.closePath();
-crc2.stroke();
+namespace L08_Canvas {
+window.addEventListener("load", handleLoad);
 
+function handleLoad(): void {
+    let canvas: HTMLCanvasElement = <HTMLCanvasElement> document.querySelector("canvas");
+    let crc2: CanvasRenderingContext2D = <CanvasRenderingContext2D> canvas.getContext("2d");
+    
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+
+    let colors: string[] = ["thistle", "sandybrown", "lightsalmon", "teal", "indianred"];
+
+    for (let index: number = 0; index < 50; index++) {
+        let x: number = Math.floor(Math.random() * Math.floor(canvas.width));
+        let y: number = Math.floor(Math.random() * Math.floor(canvas.height));
+        let x2: number = Math.floor(Math.random() * Math.floor(canvas.width));
+        let y2: number = Math.floor(Math.random() * Math.floor(canvas.height));
+        let x3: number = Math.floor(Math.random() * Math.floor(canvas.width));
+        let y3: number = Math.floor(Math.random() * Math.floor(canvas.height));
+
+        let pickColor: number = Math.floor(Math.random() * Math.floor(5));
+
+    
+        crc2.beginPath();
+        crc2.strokeStyle = colors[pickColor];
+        
+        crc2.moveTo(x, y);
+        crc2.lineTo(x2, y2);
+        crc2.lineTo(x3, y3);
+        crc2.closePath();
+        crc2.stroke();
+
+        crc2.beginPath();
+        crc2.fillStyle = colors[pickColor];
+        crc2.arc( x, y, x2, y2, 2 * Math.PI , true);
+        
+        crc2.stroke();
+      
+        
+        
+    }
+}
+
+
+
+
+
+}
