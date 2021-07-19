1b2173bf92f2561e055deefb48a6031248a10776
christinadae <christina.daeschner@hs-furtwangen.de>
2021-07-19 19:55:39 +0200

    steckbrief

--- Endabgabe_EIA2/TS/Main/main.ts
+++ Endabgabe_EIA2/TS/Main/main.ts
@@ -1,5 +1,11 @@
 namespace Endabgabe {
-
+/*
+Aufgabe: <EIA2-Endabgabe-Fußball Simulation>
+Name: <Christina Däschner>
+Matrikel: <266417>
+Datum: <19.07.2021>
+Quellen: <Zusammenarbeit mit: Huu Thien Phan Ngoc, Mona Kabelka, Timur Yildirim, Mariia Kolkutova>
+*/
     window.addEventListener("load", handleLoad);
     export let crc2: CanvasRenderingContext2D;
     export let canvas: HTMLCanvasElement;
